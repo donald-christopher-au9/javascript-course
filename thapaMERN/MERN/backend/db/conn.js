0bcf58f9f537d1aaee5ad0104f7ae0daf8e1315d
donald-christopher-au9 <67589447+donald-christopher-au9@users.noreply.github.com>
2021-04-07 14:37:41 +0530

    update

---
+++ thapaMERN/MERN/backend/db/conn.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const DB = process.env.DATABASE;
+
+mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify:false
+}).then((con) => {
+    console.log('connection successful');
+}).catch((err) => {
+    console.log("the error is "+err);
+});
+
